787b2a637f3bc429ef4a1e7bb26607ce9baeb398
sann4649 <93958204+sann4649@users.noreply.github.com>
2021-11-28 23:19:41 +0900

    Update uranai.py

--- uranai.py
+++ uranai.py
@@ -266,8 +266,6 @@ def myuranai(input_text):
     
     if 'color' == '赤色':
       return '赤色を選んだあなたは情熱的で行動力があり、とにかく一歩前へ前進する性格です。'
-    if 'color' == '黄色':
-      return '黄色を選んだあなたはとにかく明るくて、ユーモアがあり、知性溢れる性格です。'
     return 'あなたの運勢は吉'
 
   return output_text
